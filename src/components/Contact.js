120e25859e70b1bb59543f945c70a09297533a21
webermg <matthew.gerald.weber@gmail.com>
2020-11-10 20:30:45 -0800

    updated for react

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,39 @@ import React from 'react'
 export default function Contact() {
   return (
     <div>
-      <h1>Contact</h1>
+      <div class="container mt-5 mb-5">
+    <div class="col-sm-9 rounded bg-light">
+      <div class="row">
+        <div class="col-12">
+          <h1 class="py-3 border-bottom">Contact</h1>
+        </div>
+
+      </div>
+      <div class="row pb-4">
+        <div class="col-md-4 mx-3">
+          matthew.gerald.weber@gmail.com
+        </div>
+
+
+        <div class="col-md-4 mx-3">
+          Phone: 502-533-1511
+        </div>
+      </div>
+      <div class="row pb-4">
+        <div class="col-md-4 mx-3">
+          <a href="https://www.linkedin.com/in/matthew-weber-884a03122/">LinkedIn</a>
+        </div>
+        <div class="col-md-4 mx-3">
+          <a href="https://github.com/webermg">GitHub</a>
+        </div>
+      </div>
+      <div class="row pb-4">
+        <div class="col-md-4 mx-3">
+          <a href="assets/Resume.pdf">Resume</a>
+        </div>
+      </div>
+    </div>
+  </div>
     </div>
   )
 }
