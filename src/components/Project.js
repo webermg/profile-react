faaf78da39e741feedc0c62984f80208dd476b16
webermg <matthew.gerald.weber@gmail.com>
2020-11-10 20:31:49 -0800

    Projects now create from props

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function Project() {
+export default function Project(props) {
   return (
-    <div>
-      
+    <div class="card">
+      <img src={props.img} class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">{props.title}</h5>
+        <p class="card-text">{props.desc}</p>
+        <a href={props.siteLink} class="btn btn-primary">Site</a>
+        <a href={props.githubLink} class="btn btn-primary">Github</a>
+      </div>
     </div>
   )
 }
